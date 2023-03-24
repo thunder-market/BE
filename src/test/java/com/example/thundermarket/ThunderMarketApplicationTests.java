560ee510e992a86064ec1f0b89560ef0968259b2
jangjinhyeok <jangjh45@naver.com>
2023-03-24 11:28:32 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/example/thundermarket/ThunderMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thundermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
