48138c8f43973cc26d94661510967ec70f2ffa2d
jangjinhyeok <jangjh45@naver.com>
2023-03-24 20:11:18 +0900

    feat : sql auto import 추가

--- src/main/java/com/example/thundermarket/config/WebSecurityConfig.java
+++ src/main/java/com/example/thundermarket/config/WebSecurityConfig.java
@@ -50,8 +50,8 @@ public class WebSecurityConfig {
 //        일단은 회원가입, 로그인, get으로 들어오는 상품조회만 허용하고 나머지 인증 받게 설정.
 //        jwt필터와 필터예외처리필터 2개 생성
         http.authorizeRequests().antMatchers("/signup").permitAll()
-                .antMatchers( "/login").permitAll()
-                .antMatchers( "/category").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/category").permitAll()
                 .antMatchers(HttpMethod.GET, "/products/**").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
@@ -66,7 +66,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
+    public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
 
@@ -98,7 +98,6 @@ public class WebSecurityConfig {
 
         return source;
     }
-
 
 
 }
