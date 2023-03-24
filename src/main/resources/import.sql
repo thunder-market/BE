3030198f1df8c938de95320ea023cc05532c932c
jangjinhyeok <jangjh45@naver.com>
2023-03-24 22:26:56 +0900

    fix : h2 콘솔 관련 수정, DB더미 데이터 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,9 @@
+INSERT INTO USERS VALUES (1, 'user1@example.com', 123456, 'user1', 'password1', 'USER', 'name1');
+INSERT INTO USERS VALUES (2, 'user2@example.com', 789012, 'user2', 'password2', 'USER', 'name2');
+INSERT INTO USERS VALUES (3, 'user3@example.com', 456789, 'user3', 'password3', 'USER', 'name3');
+INSERT INTO USERS VALUES (4, 'user4@example.com', 901234, 'user4', 'password4', 'USER', 'name4');
+INSERT INTO USERS VALUES (5, 'user5@example.com', 345678, 'user5', 'password5', 'USER', 'name5');
+
 INSERT INTO CATEGORY VALUES(1, 100, '여성의류');
 INSERT INTO CATEGORY VALUES(2, 200, '남성의류');
 INSERT INTO CATEGORY VALUES(3, 300, '신발');
@@ -6,3 +12,14 @@ INSERT INTO CATEGORY VALUES(5, 500, '시계/주얼리');
 INSERT INTO CATEGORY VALUES(6, 600, '패션 액세서리');
 INSERT INTO CATEGORY VALUES(7, 700, '디지털/가전');
 INSERT INTO CATEGORY VALUES(8, 800, '스포츠/레저');
+
+INSERT INTO PRODUCT VALUES(1, '2022-11-07T15:30', '2022-11-08T15:30', '신발입니다.', TRUE, 'jfiejdkfiejf', FALSE, 50000, 1, TRUE, '신발 팜니다', TRUE, '1');
+INSERT INTO PRODUCT VALUES(2, '2022-11-08T10:30', '2022-11-09T12:30', '가방 팝니다', FALSE, 'dijfeijifjdi', TRUE, 30000, 1, FALSE, '새로운 가방입니다.', TRUE, '1');
+INSERT INTO PRODUCT VALUES(3, '2022-11-09T18:00', '2022-11-10T12:00', '의자 팝니다', TRUE, 'dkjfkdfjdf', TRUE, 100000, 2, TRUE, '새로운 의자입니다.', FALSE, '2');
+INSERT INTO PRODUCT VALUES(4, '2022-11-10T09:00', '2022-11-11T15:00', '핸드폰 팝니다', FALSE, 'fijefijef', TRUE, 800000, 2, TRUE, '새로운 핸드폰입니다.', TRUE, '2');
+INSERT INTO PRODUCT VALUES(5, '2022-11-11T12:00', '2022-11-12T16:00', '카메라 팝니다', TRUE, 'dijfiejfj', FALSE, 400000, 1, FALSE, '새로운 카메라입니다.', TRUE, '3');
+INSERT INTO PRODUCT VALUES(6, '2022-11-12T14:00', '2022-11-13T10:00', '의류 팝니다', FALSE, 'kdfjdfjdf', TRUE, 20000, 3, FALSE, '새로운 의류입니다.', TRUE, '4');
+INSERT INTO PRODUCT VALUES(7, '2022-11-13T11:30', '2022-11-14T09:30', '책 팝니다', TRUE, 'dijefijf', FALSE, 15000, 3, TRUE, '새로운 책입니다.', FALSE, '5');
+INSERT INTO PRODUCT VALUES(8, '2022-11-14T20:30', '2022-11-15T16:30', '데스크탑 컴퓨터 팝니다', FALSE, 'dijefijdfj', TRUE, 500000, 4, TRUE, '새로운 데스크탑 컴퓨터입니다.', TRUE, '5');
+INSERT INTO PRODUCT VALUES(9, '2022-11-15T13:00', '2022-11-16T17:00', '노트북 팝니다', TRUE, 'kdjifjedf', TRUE, 1000000, 4, FALSE, '새로운 노트북입니다.', TRUE, '4');
+INSERT INTO PRODUCT VALUES(10, '2022-11-16T10:00', '2022-11-17T08:00', '자전거 팝니다', FALSE, 'dijfiejdfj', TRUE, 1500000, 5, TRUE, '새로운 자전거입니다.', FALSE, '3');
