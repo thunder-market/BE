9471b86e3fefc095f873b9e9597bdc5fb738d012
jangjinhyeok <jangjh45@naver.com>
2023-03-25 19:36:24 +0900

    feat : 상세페이지 같은 카테고리 게시물 최신순 6개 표시

--- src/main/java/com/example/thundermarket/products/repository/ProductRepository.java
+++ src/main/java/com/example/thundermarket/products/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.example.thundermarket.products.repository;
 
 import com.example.thundermarket.products.entity.Product;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,13 +9,17 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-//    isDone이 false이고 createdAt을 기준으로 내림차순으로 정렬한 product를 모두 가져오는 쿼리를 직접 적용.
+    //    isDone이 false이고 createdAt을 기준으로 내림차순으로 정렬한 product를 모두 가져오는 쿼리를 직접 적용.
     @Query(value = "SELECT * FROM product WHERE is_done = false ORDER BY created_at DESC", nativeQuery = true)
     List<Product> findAllByIsDoneFalseOrderByCreatedAtDesc();
 
-//    isDone이 false이고 주어진 productid값이 아닌 product를 limit개수만큼 가져오는 쿼리를 직접 적용.
-    @Query(value = "SELECT * FROM product WHERE is_done = false AND id != :productId ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
+    //    isDone이 false이고 주어진 productid값이 아닌 product를 limit개수만큼 가져오는 쿼리를 직접 적용.
+    @Query(value = "SELECT * FROM product WHERE is_done = false AND id != :productId  ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
     List<Product> findLatestNotDoneProductsExceptGivenId(@Param("limit") int limit, @Param("productId") Long productId);
+
+    //    같은 카테고리 게시물 최신순 6개를 불러온다. isDone이 false이고 주어진 productid값이 아닌 product를 limit개수만큼 가져오는 쿼리를 직접 적용.
+    @Query(value = "SELECT * FROM product WHERE is_done = false AND id != :productId AND cate_code = (SELECT cate_code FROM product WHERE id = :productId) ORDER BY created_at DESC LIMIT :limit", nativeQuery = true)
+    List<Product> findLatestNotDoneProductsByCategoryIdExceptGivenId(@Param("limit") int limit, @Param("productId") Long productId);
 
     //    아래는 isDone 상관없이 간단하게 jpa 쓸때 코드
 //    List<Product> findAllByOrderByCreatedAtDesc();
--- src/main/java/com/example/thundermarket/products/service/ProductService.java
+++ src/main/java/com/example/thundermarket/products/service/ProductService.java
@@ -10,7 +10,6 @@ import com.example.thundermarket.products.repository.ProductRepository;
 import com.example.thundermarket.users.entity.User;
 import com.example.thundermarket.users.entity.UserRoleEnum;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,14 +25,14 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
 
-//    상품 작성
+    //    상품 작성
     @Transactional
     public MessageResponseDto createProduct(ProductRequestDto productRequestDto, User user) {
         productRepository.saveAndFlush(new Product(productRequestDto, user));
         return new MessageResponseDto(HttpStatus.OK, "상품이 등록되었습니다.");
     }
 
-//    전체 상품 조회
+    //    전체 상품 조회
     @Transactional(readOnly = true)
     public List<ProductListResponseDto> getProductList() {
         List<ProductListResponseDto> productListResponseDtos = new ArrayList<>();
@@ -45,7 +44,7 @@ public class ProductService {
         return productListResponseDtos;
     }
 
-//    선택 상품 조회
+    //    선택 상품 조회
     @Transactional(readOnly = true)
     public ProductDetailResponseDto getProductDetailList(Long pdid, User user) {
         Product getproduct = productRepository.findById(pdid).orElseThrow(
@@ -53,7 +52,7 @@ public class ProductService {
         );
         boolean isAuth = false;
         if (user != null) {
-            if (user.getRole() == UserRoleEnum.ADMIN || user.getId().equals(getproduct.getUser().getId())){
+            if (user.getRole() == UserRoleEnum.ADMIN || user.getId().equals(getproduct.getUser().getId())) {
                 isAuth = true;
             }
         }
@@ -61,7 +60,7 @@ public class ProductService {
 //        최신 상품 리스트 6개 같이반환
         List<ProductListResponseDto> productListResponseDtos = productRepository
 //                .findTop6ByIdNotOrderByCreatedAtDesc(pdid).stream()
-                .findLatestNotDoneProductsExceptGivenId(6,pdid).stream()
+                .findLatestNotDoneProductsByCategoryIdExceptGivenId(6, pdid).stream()
                 .map(ProductListResponseDto::new)
                 .collect(Collectors.toList());
 
@@ -82,7 +81,7 @@ public class ProductService {
     }
 
     @Transactional
-    public MessageResponseDto delete(Long pdid, User user){
+    public MessageResponseDto delete(Long pdid, User user) {
 
         Product product = productRepository.findById(pdid).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.")
