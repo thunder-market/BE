2f5f677a3e1132e74da53f5cbd400c555f76b247
ChoonB <shgkdc@gmail.com>
2023-03-27 10:50:23 +0900

    Fix : Dev에도 cors 배포주소 추가

--- src/main/java/com/example/thundermarket/config/WebSecurityConfig.java
+++ src/main/java/com/example/thundermarket/config/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig {
 
         // 프론트 로컬 서버 허용
         config.addAllowedOrigin("http://localhost:3000");
-//        config.addAllowedOrigin("나중에 프론트 배포시 주소");
+        config.addAllowedOrigin("http://clone-thunder-market.s3-website.ap-northeast-2.amazonaws.com/");
 
         // 특정 헤더를 클라이언트 측에서 사용할 수 있게 지정
         // 만약 지정하지 않는다면, Authorization 헤더 내의 토큰 값을 사용할 수 없음
