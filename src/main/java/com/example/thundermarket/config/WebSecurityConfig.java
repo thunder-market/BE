665e6414310e73db8901b6522fabb05dcd7b9d7f
jangjinhyeok <jangjh45@naver.com>
2023-03-27 15:51:42 +0900

    feat : S3 이미지 업로드 자동배보 수정
    ImageResponseDto.java  변수 이름 카멜케이스로 변경

--- src/main/java/com/example/thundermarket/config/WebSecurityConfig.java
+++ src/main/java/com/example/thundermarket/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig {
                 .antMatchers("/login").permitAll()
                 .antMatchers("/kakao/callback").permitAll()
                 .antMatchers("/category").permitAll()
-                .antMatchers("/image/**").permitAll()
+                .antMatchers("/products/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/products/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/products").permitAll()
                 .anyRequest().authenticated()
