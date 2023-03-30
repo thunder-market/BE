ea402720e953c674b20acc908599809d89260c31
parker4942 <parkmj4312@gmail.com>
2023-03-30 09:54:48 +0900

    fix : 카카오톡 로그인 리다이렉트 localhost:3000 수정

--- src/main/java/com/example/thundermarket/users/controller/KakaoController.java
+++ src/main/java/com/example/thundermarket/users/controller/KakaoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class KakaoController {
 //    카카오 로그인을 위한 컨트롤러
@@ -22,14 +22,14 @@ public class KakaoController {
 
     //    카카오 로그인 반환값 string redirect로 할지 어떻게 할지 좀 더 고민
     @GetMapping("/kakao/callback")
-    public LoginResponseDto kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         // code: 카카오 서버로부터 받은 인가 코드
         String nick = kakaoService.kakaoLogin(code, response);
         // Cookie 생성 및 직접 브라우저에 Set
 //        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
 ////        cookie.setDomain("http://clone-thunder-market.s3-website.ap-northeast-2.amazonaws.com");
 //        response.addCookie(cookie);
-        return new LoginResponseDto(nick);
+        return "redirect:localhost:3000";
 
     }
 }
